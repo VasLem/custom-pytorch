208706d66691a8b02a3aee33e93b4bd63ff89dec
vaslem <vasilis.lemonidis@metis.tech>
2019-07-23 06:05:13 +0300

    fixes in importing of external modules

--- custom_pytorch/external/object_detection_metrics/__init__.py
+++ custom_pytorch/external/object_detection_metrics/__init__.py
@@ -1,2 +1,3 @@
 import os, sys
-sys.path.append(os.sep.join(__file__.split(os.sep)[-1]))
+sys.path.append(os.sep.join(__file__.split(os.sep)[:-1]))
+import _init_paths
--- custom_pytorch/external/pytorch_enet/__init__.py
+++ custom_pytorch/external/pytorch_enet/__init__.py
@@ -1,2 +1,2 @@
 import os, sys
-sys.path.append(os.sep.join(__file__.split(os.sep)[-1]))
+sys.path.append(os.sep.join(__file__.split(os.sep)[:-1]))
