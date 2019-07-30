fe1eb4119de09d0b623860915b7256b564eccc53
vaslem <vasilis.lemonidis@metis.tech>
2019-07-30 14:55:39 +0300

    Removed wrt_batch due to poor performance

--- custom_pytorch/custom_losses/dice.py
+++ custom_pytorch/custom_losses/dice.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-import numpy as np
 from custom_pytorch.metrics import DiceCoeff
 import torch.nn.functional as F
 from custom_pytorch.losses.dice import DiceLoss as _DiceLoss
@@ -8,38 +7,45 @@ from custom_pytorch.custom_utils import apply_reduction
 
 
 __all__ = ['CEDiceLoss', 'DiceLoss', 'BCEAndDiceLoss',
-           'BCEAndCEDiceLoss', 'WindowedCEDiceLoss']
+           'BCEAndCEDiceLoss', 'WindowedCEDiceLoss', 'BCEAndWindowedCEDiceLoss']
 
 
 class DiceLoss(_DiceLoss):
     __name__ = 'dice_loss'
 
+    def __init__(self, with_logits, pos_weight=None, threshold=None):
+        activation = None
+        if with_logits:
+            activation = nn.Sigmoid()
+        super().__init__(pos_weight=pos_weight, threshold=threshold, activation=activation)
+
 
 class WindowedCEDiceLoss(nn.Module):
     __name__ = 'windowed_ce_dice_loss'
 
-    def __init__(self, *args, red_kernel_size=3, **kwargs):
+    def __init__(self, with_logits=True, red_kernel_size=3, pos_weight=None, threshold=None):
         super().__init__()
-        self.dice_coeff = DiceCoeff(*args, **kwargs)
+        self.activation = None
+        if with_logits:
+            self.activation = nn.Sigmoid()
+        self.dice_coeff = DiceCoeff(
+            activation=None, pos_weight=pos_weight, threshold=threshold)
         self.red_kernel_size = red_kernel_size
         self.kernel = torch.ones(
-            (1, 1, red_kernel_size, red_kernel_size)) * 1/(red_kernel_size**2)
-        self.pad = nn.ConstantPad2d((self.red_kernel_size - 1) // 2, 1)
-        self.mask_pad = nn.ConstantPad2d((self.red_kernel_size - 1) // 2, 0)
+            (1, 1, red_kernel_size, red_kernel_size)) / float(red_kernel_size ** 2)
+        self.pad = nn.ConstantPad2d((self.red_kernel_size - 1) // 2, 0)
 
     def forward(self, input, target, reduction='mean'):
-        self.kernel = self.kernel.to(input.device)
+        if self.activation is not None:
+            input = self.activation(input)
+        kernel = self.kernel.to(input.device)
+        input = self.pad(F.conv2d(input,
+                                  kernel))
+        target = self.pad(F.conv2d(target, kernel))
         pos_coeffs = self.dice_coeff(
-            input, target, wrt_batch=True, reduction='none', _no_reduce=True)
-        pos_coeffs = self.pad(F.conv2d(pos_coeffs,
-                                       self.kernel))
+            input, target, _no_reduce=True)
         neg_coeffs = self.dice_coeff(
-            1 - self.dice_coeff.activation(input),
-            1 - target, wrt_batch=True, reduction='none', _no_reduce=True,
-            do_activation=False)
-        neg_coeffs = self.pad(F.conv2d(neg_coeffs,
-                                       self.kernel))
-        # remove the outer bounds from the play -> a necessary drawback of this loss
+            1 - input, 1 - target, _no_reduce=True)
         loss = - (torch.log2(pos_coeffs + 1e-7) +
                   torch.log2(neg_coeffs + 1e-7))
 
@@ -49,33 +55,36 @@ class WindowedCEDiceLoss(nn.Module):
 class CEDiceLoss(nn.Module):
     __name__ = 'ce_dice_loss'
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, with_logits=True, pos_weight=None, threshold=None):
         super().__init__()
-        self.dice_coeff = DiceCoeff(*args, **kwargs)
+        self.activation = None
+        if with_logits:
+            self.activation = nn.Sigmoid()
+        self.dice_coeff = DiceCoeff(
+            activation=None, pos_weight=pos_weight, threshold=threshold)
 
-    def forward(self, input, target, wrt_batch=True, reduction='mean'):
+    def forward(self, input, target):
+        if self.activation is not None:
+            input = self.activation(input)
         pos_coeffs = self.dice_coeff(
-            input, target, wrt_batch=wrt_batch, reduction='none')
+            input, target)
         neg_coeffs = self.dice_coeff(
-            1 - self.dice_coeff.activation(input),
-            1 - target, wrt_batch=wrt_batch, reduction='none',
-            do_activation=False)
+            1 - input, 1 - target)
         loss = - (torch.log2(pos_coeffs + 1e-7) +
                   torch.log2(neg_coeffs + 1e-7))
-        if wrt_batch:
-            return apply_reduction(loss, reduction)
         return loss
 
 
 class BCEAndDiceLoss(nn.Module):
     __name__ = 'bce_and_dice_loss'
 
-    def __init__(self, with_logits=True, dice_loss_kwargs={}, bce_kwargs={}):
+    def __init__(self, with_logits=True, dice_loss_kwargs={}, bce_kwargs={}, _dice_loss_class=None):
         super().__init__()
         self.with_logits = with_logits
-        if with_logits:
-            dice_loss_kwargs['activation'] = 'sigmoid'
-        self.dice_loss = DiceLoss(**dice_loss_kwargs)
+        if _dice_loss_class is None:
+            _dice_loss_class = DiceLoss
+        self.dice_loss = _dice_loss_class(
+            with_logits=with_logits, **dice_loss_kwargs)
         if self.with_logits:
             self.bce_loss = torch.nn.BCEWithLogitsLoss(**bce_kwargs)
         else:
@@ -90,5 +99,13 @@ class BCEAndCEDiceLoss(BCEAndDiceLoss):
     __name__ = 'bce_and_ce_dice_loss'
 
     def __init__(self, with_logits=True, dice_loss_kwargs={}, bce_kwargs={}):
-        super().__init__(with_logits, dice_loss_kwargs, bce_kwargs)
-        self.dice_loss = CEDiceLoss(**dice_loss_kwargs)
+        super().__init__(with_logits, dice_loss_kwargs,
+                         bce_kwargs, _dice_loss_class=CEDiceLoss)
+
+
+class BCEAndWindowedCEDiceLoss(BCEAndDiceLoss):
+    __name__ = 'bce_and_windowed_ce_dice_loss'
+
+    def __init__(self, with_logits=True, dice_loss_kwargs={}, bce_kwargs={}):
+        super().__init__(with_logits, dice_loss_kwargs,
+                         bce_kwargs, _dice_loss_class=WindowedCEDiceLoss)
--- custom_pytorch/metrics/dice_coeff.py
+++ custom_pytorch/metrics/dice_coeff.py
@@ -1,10 +1,9 @@
 import torch
 from torch import nn
-from custom_pytorch.custom_utils import apply_reduction
 
 
 class DiceCoeff(nn.Module):
-    def __init__(self, pos_weight=None, threshold=None, activation='sigmoid'):
+    def __init__(self, pos_weight=None, threshold=None, activation=None):
         """
         :param pos_weight: Positional weights given to each element of the tensor,
             defaults to None
@@ -35,18 +34,18 @@ class DiceCoeff(nn.Module):
                 raise ValueError("Supplied threshold is not numeric")
         self.threshold = threshold
 
-    def _compute(self, input, target, wrt_batch, no_reduce=False, do_activation=True):
-        smooth = 1e-7
+    def _compute(self, input, target, no_reduce=False):
+        smooth = 1.
 
-        if self.activation is not None and do_activation:
+        if self.activation is not None:
             input = self.activation(input)
 
         if not no_reduce:
-            iflat = input.contiguous().view(input.size()[0], -1)
-            tflat = target.contiguous().view(input.size()[0], -1)
+            iflat = input.view(input.size()[0], -1)
+            tflat = target.view(input.size()[0], -1)
         else:
-            iflat = input.contiguous()
-            tflat = target.contiguous()
+            iflat = input
+            tflat = target
         if self.pos_weight is not None and not no_reduce:
             self.pos_weight = self.pos_weight.to(input.device)
             iflat = iflat * self.pos_weight.view(1, -1)
@@ -55,34 +54,19 @@ class DiceCoeff(nn.Module):
             iflat = (iflat >= self.threshold).float()
         intersection = (iflat * tflat)
         addition = iflat + tflat
-        if no_reduce:
-            return ((2. * intersection + smooth) /
-                    (addition + smooth))
-        if wrt_batch:
-            intersection = torch.sum(intersection, dim=1)
-            addition = torch.sum(addition, dim=1)
-        else:
+        if not no_reduce:
             intersection = intersection.sum()
             addition = addition.sum()
         return ((2. * intersection + smooth) /
                 (addition + smooth))
 
-    def forward(self, input, target, wrt_batch=True, reduction='mean', _no_reduce=False,
-                do_activation=True):
+    def forward(self, input, target, _no_reduce=False):
         """
         :param input: the input
         :type input: Tensor
         :param target: the target
         :type target: Tensor
-        :param wrt_batch: whether to calculate dice coefficient
-            for each sample in the batch separately, defaults to True
-        :type wrt_batch: bool, optional
-        :param reduction: if `wrt_batch` is provided, the provided reduction will be performed,
-            only available is `mean`, `sum` or `none`, defaults to `mean`
-        :type reduction: str, optional
         """
-        coeff = self._compute(input, target, wrt_batch,
-                              no_reduce=_no_reduce, do_activation=do_activation)
-        if wrt_batch and not _no_reduce:
-            return apply_reduction(coeff, reduction)
+        coeff = self._compute(input, target,
+                              no_reduce=_no_reduce)
         return coeff
